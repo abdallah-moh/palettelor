4d2119e6b89715509975391168039c4ce71249ad
abdallah-moh1 <abdallah.moh.q@gamil.com>
2022-02-20 01:31:39 +0200

    Add build, format and lint workflows

--- src/main/modules/module-model.ts
+++ src/main/modules/module-model.ts
@@ -3,18 +3,15 @@ import os from 'os';
 class Module {
   protected loaded: boolean;
   protected platforms: string[];
-  protected dirname: string;
 
-  constructor(dirname: string) {
+  constructor() {
     this.loaded = false;
-    this.dirname = dirname;
     this.platforms = ['win32', 'linux', 'darwin'];
   }
 
   // To not be overridden
   async init(): Promise<void> {
-    if (this.loaded)
-      throw new TypeError(`Module ${this.constructor.name} is already loaded`);
+    if (this.loaded) throw new TypeError(`Module ${this.constructor.name} is already loaded`);
 
     if (this.platforms.includes(os.platform())) {
       await this.load().catch((err) => {
@@ -28,9 +25,7 @@ class Module {
 
   // Can (now) be an asynchronous method
   async load(): Promise<void> {
-    throw new TypeError(
-      `Module ${this.constructor.name} should have a load() method`
-    );
+    throw new TypeError(`Module ${this.constructor.name} should have a load() method`);
     // Do whatever you want here :)
   }
 }
